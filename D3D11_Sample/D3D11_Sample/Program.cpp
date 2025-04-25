41f23eae5683bb2e5c0dbfe561f5f3dd450320fe
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2025-04-25 17:53:51 +0900

    List0104(ウィンドウの作成)

--- D3D11_Sample/D3D11_Sample/Program.cpp
+++ D3D11_Sample/D3D11_Sample/Program.cpp
@@ -1,17 +1,27 @@
 #include <Windows.h>
 
 int WINAPI wWinMain(
-	_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine,_In_ int nCmdShow)
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
-	// MB_OKCANCELを指定すると『OK』ボタンと『CANCEL』ボタンを表示
-	if (MessageBox(NULL, L"ここにメッセージが表示されます。", L"質問",
-		MB_OKCANCEL) == IDOK) {
-		MessageBox(NULL, L"OKされました。", L"結果", MB_OK);
-	}
-	else {
-		MessageBox(NULL, L"キャンセルされました。", L"結果", MB_OK);
-	}
+    HWND hWnd = NULL;
 
-	return 0;
+    hWnd = CreateWindowExW(
+        0,  L"Static",L"タイトル",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,640, 480,
+        NULL,NULL,hInstance,NULL);
+    if (hWnd == NULL) {
+        MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"エラー", MB_OK);
+        return -1;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MessageBox(hWnd, L"ウィンドウが表示されましたか？", L"確認", MB_OK);
+
+    return 0;
 }
