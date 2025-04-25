cb8f0e5e65901cf25d44398396e65c26979b0c7a
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2025-04-25 17:36:50 +0900

    List0103(メッセージボックスの戻り値)

--- D3D11_Sample/D3D11_Sample/Program.cpp
+++ D3D11_Sample/D3D11_Sample/Program.cpp
@@ -4,8 +4,14 @@ int WINAPI wWinMain(
 	_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,_In_ int nCmdShow)
 {
-	// メッセージボックスを表示
-	MessageBoxW(NULL,L"ここにメッセージが表示されます。",L"タイトル", MB_ABORTRETRYIGNORE);
+	// MB_OKCANCELを指定すると『OK』ボタンと『CANCEL』ボタンを表示
+	if (MessageBox(NULL, L"ここにメッセージが表示されます。", L"質問",
+		MB_OKCANCEL) == IDOK) {
+		MessageBox(NULL, L"OKされました。", L"結果", MB_OK);
+	}
+	else {
+		MessageBox(NULL, L"キャンセルされました。", L"結果", MB_OK);
+	}
 
 	return 0;
 }
