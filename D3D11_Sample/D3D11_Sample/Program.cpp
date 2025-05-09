a2729e646ea1888662cf157f6a34ca54822ef7bc
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-05-09 15:58:49 +0900

    List0106(ウィンドウクラスの登録)

--- D3D11_Sample/D3D11_Sample/Program.cpp
+++ D3D11_Sample/D3D11_Sample/Program.cpp
@@ -1,31 +1,36 @@
 #include <Windows.h>
 
+HWND hWnd = NULL;
+
 int WINAPI wWinMain(
 	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	HWND hWnd = NULL;
+	const wchar_t CLASS_NAME[] = L"GameWindow";
+	WNDCLASSEXW wndClass = {};
+	wndClass.cbSize = sizeof(WNDCLASSEXW);
+	wndClass.lpfnWndProc = DefWindowProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+	wndClass.lpszClassName = CLASS_NAME;
+	if (!RegisterClassExW(&wndClass)) {
+		MessageBox(NULL, L"ウィンドウ クラスの登録に失敗しました。", L"エラー", MB_OK);
+		return 0;
+	}
 
 	RECT rect = { 0, 0, 640, 480 };
-	AdjustWindowRectEx(
-		&rect,
-		WS_OVERLAPPEDWINDOW,
-		FALSE,
-		0);
+	AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, FALSE, 0);
 
-	hWnd = CreateWindowExW(
-		0,
-		L"Static",
+	hWnd = CreateWindowExW(0,
+		CLASS_NAME,
 		L"タイトル",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		(rect.right - rect.left), (rect.bottom - rect.top),
-		NULL,
-		NULL,
-		hInstance, NULL);
+		NULL, NULL, hInstance, NULL);
 	if (hWnd == NULL) {
 		MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"エラー", MB_OK);
-		return false;
+		return 0;
 	}
 
 	ShowWindow(hWnd, nCmdShow);
