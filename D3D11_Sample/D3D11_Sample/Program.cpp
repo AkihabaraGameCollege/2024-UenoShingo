6a6f038f3b16bda6b28bd4c192c3524324cd22ce
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-05-09 15:32:48 +0900

    List0105(クライアント領域)

--- D3D11_Sample/D3D11_Sample/Program.cpp
+++ D3D11_Sample/D3D11_Sample/Program.cpp
@@ -1,27 +1,37 @@
 #include <Windows.h>
 
 int WINAPI wWinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR lpCmdLine,
-    _In_ int nCmdShow)
+	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    HWND hWnd = NULL;
+	HWND hWnd = NULL;
 
-    hWnd = CreateWindowExW(
-        0,  L"Static",L"タイトル",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,640, 480,
-        NULL,NULL,hInstance,NULL);
-    if (hWnd == NULL) {
-        MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"エラー", MB_OK);
-        return -1;
-    }
+	RECT rect = { 0, 0, 640, 480 };
+	AdjustWindowRectEx(
+		&rect,
+		WS_OVERLAPPEDWINDOW,
+		FALSE,
+		0);
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+	hWnd = CreateWindowExW(
+		0,
+		L"Static",
+		L"タイトル",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		(rect.right - rect.left), (rect.bottom - rect.top),
+		NULL,
+		NULL,
+		hInstance, NULL);
+	if (hWnd == NULL) {
+		MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"エラー", MB_OK);
+		return false;
+	}
 
-    MessageBox(hWnd, L"ウィンドウが表示されましたか？", L"確認", MB_OK);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-    return 0;
+	MessageBox(hWnd, L"ウィンドウが表示されましたか？", L"確認", MB_OK);
+
+	return 0;
 }
