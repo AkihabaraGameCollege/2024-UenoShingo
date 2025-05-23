bb29fd62b7d1de58cf58c55ee07f0adeb8c3d71a
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-05-23 16:21:41 +0900

    List0303(バックバッファーとレンダーターゲット)2

--- D3D11_Sample/D3D11_Sample/Game.cpp
+++ D3D11_Sample/D3D11_Sample/Game.cpp
@@ -99,6 +99,13 @@ int Game::Run()
 	Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
 	// レンダーターゲット
 	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
+	// バックバッファーをシェーダーで利用するためのリソース ビュー
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> renderTargetResourceView;
+	// 深度ステンシルのフォーマット
+	//const DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	const DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D32_FLOAT_S8X24_UINT;
+	// 深度ステンシル
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthStencilView;
 	// 画面クリアーに使用するカラー
 	FLOAT clearColor[] = { 53 / 255.0f, 70 / 255.0f, 166 / 255.0f, 1.0f };
 
@@ -128,7 +135,9 @@ int Game::Run()
 	swapChainDesc.BufferDesc.RefreshRate = { 60, 1 };
 	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	swapChainDesc.SampleDesc = { 1, 0 };
-	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferUsage =
+		DXGI_USAGE_RENDER_TARGET_OUTPUT |
+		DXGI_USAGE_SHADER_INPUT;	// シェーダーリソースとして使用することを設定
 	swapChainDesc.BufferCount = 2;
 	swapChainDesc.OutputWindow = hWnd;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
@@ -148,19 +157,78 @@ int Game::Run()
 	}
 
 	// スワップチェーンからバックバッファーを取得
-	ID3D11Texture2D* backBuffer = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> backBuffer;
 	hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
 	if (FAILED(hr)) {
 		MessageBoxW(hWnd, L"バックバッファーを取得できませんでした。", L"エラー", MB_OK);
 		return 0;
 	}
 	// バックバッファーにアクセスするためのレンダーターゲット ビューを作成
-	hr = graphicsDevice->CreateRenderTargetView(backBuffer, NULL, &renderTargetView);
+	hr = graphicsDevice->CreateRenderTargetView(backBuffer.Get(), NULL, &renderTargetView);
 	if (FAILED(hr)) {
 		MessageBoxW(hWnd, L"レンダーターゲット ビューを作成できませんでした。", L"エラー", MB_OK);
 		return 0;
 	}
-	backBuffer->Release();
+	// バックバッファーにシェーダーからアクセスするためのリソース ビューを作成
+	hr = graphicsDevice->CreateShaderResourceView(backBuffer.Get(), NULL, &renderTargetResourceView);
+	if (FAILED(hr)) {
+		MessageBoxW(hWnd, L"レンダーターゲット リソース ビューを作成できませんでした。", L"エラー", MB_OK);
+		return 0;
+	}
+	backBuffer.Reset();
+
+
+	// テクスチャのフォーマットを設定
+	DXGI_FORMAT textureFormat = depthStencilFormat;
+	switch (depthStencilFormat) {
+	case DXGI_FORMAT_D16_UNORM:
+		textureFormat = DXGI_FORMAT_R16_TYPELESS;
+		break;
+	case DXGI_FORMAT_D24_UNORM_S8_UINT:
+		textureFormat = DXGI_FORMAT_R24G8_TYPELESS;
+		break;
+	case DXGI_FORMAT_D32_FLOAT:
+		textureFormat = DXGI_FORMAT_R32_TYPELESS;
+		break;
+	case DXGI_FORMAT_D32_FLOAT_S8X24_UINT:
+		textureFormat = DXGI_FORMAT_R32G8X24_TYPELESS;
+		break;
+	}
+	// 深度ステンシルを作成
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencil;
+	D3D11_TEXTURE2D_DESC depthStencilDesc = {};
+	depthStencilDesc.Width = swapChainDesc.BufferDesc.Width;
+	depthStencilDesc.Height = swapChainDesc.BufferDesc.Height;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = textureFormat;
+	depthStencilDesc.SampleDesc = swapChainDesc.SampleDesc;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+	hr = graphicsDevice->CreateTexture2D(&depthStencilDesc, NULL, &depthStencil);
+	if (FAILED(hr)) {
+		MessageBoxW(hWnd, L"深度ステンシルを作成できませんでした。", L"エラー", MB_OK);
+		return 0;
+	}
+	// 深度ステンシルにアクセスするためのビューを作成
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
+	depthStencilViewDesc.Format = depthStencilFormat;
+	if (depthStencilDesc.SampleDesc.Count > 0) {
+		depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+	}
+	else {
+		depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		depthStencilViewDesc.Texture2D.MipSlice = 0;
+	}
+	depthStencilViewDesc.Texture2D.MipSlice = 0;
+	hr = graphicsDevice->CreateDepthStencilView(depthStencil.Get(), &depthStencilViewDesc, &depthStencilView);
+	if (FAILED(hr)) {
+		MessageBoxW(hWnd, L"深度ステンシル ビューを作成できませんでした。", L"エラー", MB_OK);
+		return 0;
+	}
+	depthStencil.Reset();
 
 
 	// メッセージループを実行
@@ -177,10 +245,12 @@ int Game::Run()
 		}
 
 		// レンダーターゲットを設定
-		ID3D11RenderTargetView* renderTargetViews[1] = { renderTargetView.Get(), };
-		immediateContext->OMSetRenderTargets(std::size(renderTargetViews), renderTargetViews, nullptr);
+		ID3D11RenderTargetView* renderTargetViews[] = { renderTargetView.Get(), };
+		immediateContext->OMSetRenderTargets(std::size(renderTargetViews), renderTargetViews, depthStencilView.Get());
 		// 画面をクリアー
 		immediateContext->ClearRenderTargetView(renderTargetView.Get(), clearColor);
+		immediateContext->ClearDepthStencilView(depthStencilView.Get(),
+			D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
 		// Direct3Dによる描画処理
 
