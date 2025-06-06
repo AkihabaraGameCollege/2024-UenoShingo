5f33d4a7acd4529f8101b0879724e81b842b0cab
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-06-06 16:30:06 +0900

    * [work]

--- D3D11_Sample/D3D11_Sample/StandardPixelShader.hlsl
+++ D3D11_Sample/D3D11_Sample/StandardPixelShader.hlsl
@@ -1,4 +1,13 @@
-float4 main() : SV_TARGET
+// ピクセル シェーダーへの入力
+struct PixelShaderInput
 {
-    return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 position : SV_POSITION;
+    float4 normal : NORMAL;
+    float2 texCoord : TEXCOORD;
+};
+
+float4 main(PixelShaderInput input) : SV_TARGET
+{
+	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    return float4(input.texCoord.x, input.texCoord.y, 0.0f, 1.0f);
 }
--- D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
+++ D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
@@ -1,4 +1,24 @@
-float4 main(float4 position : POSITION, float4 normal : NORMAL, float2 texCoord : TEXCOORD) : SV_POSITION
+// 頂点シェーダーへの入力
+struct VertexShaderInput
 {
-    return position;
+    float4 position : POSITION;
+    float4 normal : NORMAL;
+    float2 texCoord : TEXCOORD;
+};
+
+// 頂点シェーダーからの出力
+struct VertexShaderOutput
+{
+    float4 position : SV_POSITION;
+    float4 normal : NORMAL;
+    float2 texCoord : TEXCOORD;
+};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    output.position = input.position;
+    output.normal = input.normal;
+    output.texCoord = input.texCoord;
+    return output;
 }
