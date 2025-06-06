03b487044a2918c190cd223409b09dd879f36e4a
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-06-06 16:52:34 +0900

    [change] Use HLSL struct

--- D3D11_Sample/D3D11_Sample/StandardPixelShader.hlsl
+++ D3D11_Sample/D3D11_Sample/StandardPixelShader.hlsl
@@ -8,6 +8,5 @@ struct PixelShaderInput
 
 float4 main(PixelShaderInput input) : SV_TARGET
 {
-	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
-    return float4(input.texCoord.x, input.texCoord.y, 0.0f, 1.0f);
+    return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
