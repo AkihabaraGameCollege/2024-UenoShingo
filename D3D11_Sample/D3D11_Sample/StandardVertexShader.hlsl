4ce28cf6d5bc320c8722349d239ac2b9c1d4364a
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-06-19 17:26:25 +0900

    [update] Projection Transform - Orthographic Projection

--- D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
+++ D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
@@ -6,21 +6,26 @@ VertexShaderOutput main(VertexShaderInput input)
     output.position = mul(input.position, WorldMatrix);
     output.position = mul(output.position, ViewMatrix);
     
-	// プロジェクション変換後のz座標が[0.0f, 1.0f]の範囲となるようにスケーリング
+	// 正射影(Orthographic Projection)
+	// 遠近法を使用せずに射影する方法。
+	// widthとheightは自由に設定できるが、ほとんどのケースでスクリーン画面の
+	// 幅と高さの比率(アスペクト比 = 今回は4:3)で指定する。
+    const float width = 4; // クリップ面の幅
+    const float height = 3; // クリップ面の高さ
     const float near = 0.3f; // nearクリップ面
     const float far = 1000.0f; // farクリップ面
-    output.position.x = output.position.x;
-    output.position.y = output.position.y;
+	// 幅と高さを1/2しているのはposition.x、yを[-1.0f, 1.0f]の範囲に整えるため。
+    output.position.x = output.position.x / (width / 2);
+    output.position.y = output.position.y / (height / 2);
     output.position.z = (output.position.z - near) / (far - near);
     output.position.w = output.position.w;
 	/*
 	// 行列で計算する場合
-	//output.position.z = output.position.z / (far - near) - near / (far - near);
 	const float4x4 Projection = {
-		1.0f, 0.0f,                 0.0f, 0.0f,
-		0.0f, 1.0f,                 0.0f, 0.0f,
-		0.0f, 0.0f,     1 / (far - near), 0.0f,
-		0.0f, 0.0f, -near / (far - near), 1.0f,
+		 2 / width,       0.0f,                 0.0f, 0.0f,
+			  0.0f, 2 / height,                 0.0f, 0.0f,
+			  0.0f,       0.0f,     1 / (far - near), 0.0f,
+			  0.0f,       0.0f, -near / (far - near), 1.0f,
 	};
 	output.position = mul(output.position, Projection);
 	*/
