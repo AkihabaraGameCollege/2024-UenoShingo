dc5fd37c87494be644d9e39ee8922ee6efbd1aee
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-06-20 16:18:50 +0900

    [update] Projection Transform - Perspective Projection

--- D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
+++ D3D11_Sample/D3D11_Sample/StandardVertexShader.hlsl
@@ -1,66 +1,90 @@
 #include "StandardShader.hlsli"
 
+// これらはすべて定数であり、すべての頂点で固定値となる
+static const float aspect = 480.0f / 640.0f; // スクリーン画面のアスペクト比
+static const float near = 0.3f; // nearクリップ面
+static const float far = 1000.0f; // farクリップ面
+static const float fovY = radians(60); // 視錐台の垂直方向の角度（Field Of View）
+
 VertexShaderOutput main(VertexShaderInput input)
 {
-    VertexShaderOutput output;
-    output.position = mul(input.position, WorldMatrix);
-    output.position = mul(output.position, ViewMatrix);
-    
-	//// クリップ面のy座標の範囲を[-1.0f, 1.0f]として、x座標がアスペクト比を
-	//// 維持するようにスケーリング
-	//const float screenWidth = 640;	// スクリーン画面の幅
-	//const float screenHeight = 480;	// スクリーン画面の高さ
-	//const float aspect = screenHeight / screenWidth;	// スクリーン画面のアスペクト比
-	//const float near = 0.3f;	// nearクリップ面
-	//const float far = 1000.0f;	// farクリップ面
-	//output.position.x = output.position.x * aspect;
-	//output.position.y = output.position.y;
-	//output.position.z = (output.position.z - near) / (far - near);
-	//output.position.w = output.position.w;
-	///*
-	//// 行列で計算する場合
-	////output.position.z = output.position.z / (far - near) - near / (far - near);
-	//const float4x4 Projection = {
-	//	aspect, 0.0f,                 0.0f, 0.0f,
-	//	  0.0f, 1.0f,                 0.0f, 0.0f,
-	//	  0.0f, 0.0f,     1 / (far - near), 0.0f,
-	//	  0.0f, 0.0f, -near / (far - near), 1.0f,
-	//};
-	//output.position = mul(output.position, Projection);
-	//*/
+    const float4 worldPosition = mul(input.position, WorldMatrix);
+    const float4 viewPosition = mul(worldPosition, ViewMatrix);
 
-	// ビュー空間のz座標で除算することで、カメラから近い頂点は拡大され遠いものは縮小される。
-	// このとき単純にz座標で除算するとy座標が[-1.0f, 1.0f]の範囲に収まらなくなる。
-	// そこで視錐台(Viewing Frustum)の垂直方向の角度(Field Of View)を指定して、その範囲内が
-	// 描画されるようにスケーリングする。
-    const float aspect = 480.0f / 640.0f; // スクリーン画面のアスペクト比
-    const float near = 0.3f; // nearクリップ面
-    const float far = 1000.0f; // farクリップ面
-	// 視錐台の垂直方向の角度(Field Of View)
-    const float fovY = radians(60);
-	// position.zの位置での視錐台上端のy座標
-    const float frustumTop = tan(fovY / 2) * output.position.z;
-	// position.y座標をfrustumTopで除算することにより[-1.0f, 1.0f]の範囲にスケーリングする。
-    output.position.x = output.position.x * aspect / frustumTop;
-    output.position.y = output.position.y / frustumTop;
-    output.position.z = (output.position.z - near) / (far - near);
-    output.position.w = output.position.w;
-	/*
-	// 行列で計算する場合
-	// プロジェクション変換行列はCPU側で計算して定数バッファーに設定するのが定石となる。
-	// このため定数ではないoutput.position.zは行列内に含むことはできない。
-	const float4x4 Projection = {
-		aspect / tan(fovY / 2),              0.0f,                 0.0f, 0.0f,
-		                  0.0f, 1 / tan(fovY / 2),                 0.0f, 0.0f,
-		                  0.0f,              0.0f,     1 / (far - near), 0.0f,
-		                  0.0f,              0.0f, -near / (far - near), 1.0f,
-	};
-	// プロジェクション変換とは別にoutput.position.zの除算を追加する
-	output.position.x /= output.position.z;
-	output.position.y /= output.position.z;
-	output.position = mul(output.position, Projection);
-	*/
+    float4 position; // プロジェクション変換後の位置座標
+
+	//// 【Step.1】
+	//position.x = viewPosition.x * aspect / tan(fovY / 2) / viewPosition.z;
+	//position.y = viewPosition.y / tan(fovY / 2) / viewPosition.z;
+	//// プロジェクション空間の座標はピクセルシェーダーに渡される前に
+	//// (x, y, z, w) => (x / w, y / w, z / w, 1)に変換される。
+	//// w座標にビュー空間のz座標を設定しておくことで、プロジェクション変換後に
+	//// xyz座標を定数ではないビュー空間のz座標によって除算できる。
+	//// ただし、この方法はxとy座標についてはスマートな方法なのだがz座標まで
+	//// 除算されることが問題となる。（範囲が[0.0f, 1.0f]ではなくなる）
+	//// そこで先にz軸についてビュー空間のz座標で割り算しても、値が[0.0f, 1.0f]の
+	//// 範囲となるように考えてみる。
+
+	//// z軸についてはnearとfarのクリップ面の間で、すでに[0.0f, 1.0f]の範囲に
+	//// スケーリングされている。   ⇒ (viewPosition.z - near) / (far - near)
+	////
+	//// このため追加でビュー空間のz座標で除算すると、プロジェクション変換後の
+	//// z座標は[0.0f, 1.0f]の範囲に収まらなくなる。
+	//// この問題に対処するために、ビュー空間のz座標をfarで除算して[0.0f, 1.0f]の
+	//// 範囲に収めたパラメーターを考える。         ⇒ (viewPosition.z / far)
+	////
+	//// ともに[0.0f, 1.0f]の範囲にスケーリングされた値同士の除算は、
+	//// その計算結果も[0.0f, 1.0f]の範囲に収まる。
+	//// 以下の式でviewPosition.zがnearの場合は0.0f、farの場合は1.0fとなる。
+	//position.z = (viewPosition.z - near) / (far - near) / (viewPosition.z / far);
+	//position.w = viewPosition.w;
+
+	//// 【Step.2】
+	//position.x = viewPosition.x * aspect / tan(fovY / 2) / viewPosition.z;
+	//position.y = viewPosition.y / tan(fovY / 2) / viewPosition.z;
+	//position.z = (viewPosition.z - near) / (far - near) * far / viewPosition.z;
+	//position.w = viewPosition.w;
+
+	//// 【Step.3】
+	//position.x = viewPosition.x * aspect / tan(fovY / 2);
+	//position.y = viewPosition.y / tan(fovY / 2);
+	//position.z = (viewPosition.z - near) / (far - near) * far;
+	//position.x /= viewPosition.z;
+	//position.y /= viewPosition.z;
+	//position.z /= viewPosition.z;
+	//position.w = viewPosition.w;
+
+	//// 【Step.4】
+	//position.x = viewPosition.x * aspect / tan(fovY / 2);
+	//position.y = viewPosition.y / tan(fovY / 2);
+	//position.z = (viewPosition.z - near) / (far - near) * far;
+	//// w座標にビュー空間のz座標を設定
+	//position.w = viewPosition.z;
+
+	//// 【Step.5】
+	//const float4x4 Projection = {
+	//	aspect / tan(fovY / 2),              0.0f,                       0.0f, 0.0f,
+	//	                  0.0f, 1 / tan(fovY / 2),                       0.0f, 0.0f,
+	//	                  0.0f,              0.0f,     1 / (far - near) * far, 0.0f,
+	//	                  0.0f,              0.0f, -near / (far - near) * far, 1.0f,
+	//};
+	//position = mul(viewPosition, Projection);
+	//position.w = viewPosition.z;
+
+	// 【Step.6】 パースペクティブ射影変換行列の完成
+	// この行列はすべて定数で表現されているため、すべての頂点で毎回計算しなくても
+	// 事前に用意しておくことができる。
+    const float4x4 Projection =
+    {
+        aspect / tan(fovY / 2), 0.0f, 0.0f, 0.0f,
+		                  0.0f, 1 / tan(fovY / 2), 0.0f, 0.0f,
+		                  0.0f, 0.0f, 1 / (far - near) * far, 1.0f, // _34 = 1, _44 = 0によって
+		                  0.0f, 0.0f, -near / (far - near) * far, 0.0f, // w座標にz座標がセットされる
+    };
+    position = mul(viewPosition, Projection);
     
+    VertexShaderOutput output;
+    output.position = position;
     output.color = input.color;
     return output;
 }
