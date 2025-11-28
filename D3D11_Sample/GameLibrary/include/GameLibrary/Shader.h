46d36030d67809fe7d54f528e5e485a6121c3f1d
UenoShingo <ueno_shingo2002@yahoo.co.jp>
2025-11-28 13:32:20 +0900

    [add] Add BaseShader template

--- D3D11_Sample/GameLibrary/include/GameLibrary/Shader.h
+++ D3D11_Sample/GameLibrary/include/GameLibrary/Shader.h
@@ -7,53 +7,68 @@
 namespace GameLibrary
 {
 	/// <summary>
+	/// シェーダーのネイティブ ポインターを管理するクラスを作成するテンプレートを表します。
+	/// </summary>
+	/// <typeparam name="T">ネイティブ ポインターの型</typeparam>
+	template<class T>
+	class BaseShader : public GraphicsResource
+	{
+	public:
+		BaseShader(ID3D11Device5* graphicsDevice);
+		virtual ~BaseShader() = default;
+
+		T* GetNativePointer() const noexcept;
+
+	protected:
+		Microsoft::WRL::ComPtr<T> shader;
+	};
+
+	template<class T>
+	inline BaseShader<T>::BaseShader(ID3D11Device5* graphicsDevice)
+		: GraphicsResource(graphicsDevice)
+	{
+
+	}
+
+	template<class T>
+	inline T* BaseShader<T>::GetNativePointer() const noexcept
+	{
+		return shader.Get();
+	}
+
+	/// <summary>
 	/// 頂点シェーダーを表します。
 	/// </summary>
-	class VertexShader final : public GraphicsResource
+	class VertexShader final : public BaseShader<ID3D11VertexShader>
 	{
 	public:
 		VertexShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength);
 		~VertexShader() = default;
 
-		ID3D11VertexShader* GetNativePointer() const noexcept;
-
 		void Apply(ID3D11DeviceContext4* deviceContext) noexcept;
-
-	private:
-		Microsoft::WRL::ComPtr<ID3D11VertexShader> shader;
 	};
 
 	/// <summary>
 	/// ジオメトリ シェーダーを表します。
 	/// </summary>
-	class GeometryShader final : public GraphicsResource
+	class GeometryShader final : public BaseShader<ID3D11GeometryShader>
 	{
 	public:
 		GeometryShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength);
 		~GeometryShader() = default;
 
-		ID3D11GeometryShader* GetNativePointer() const noexcept;
-
 		void Apply(ID3D11DeviceContext4* deviceContext) noexcept;
-
-	private:
-		Microsoft::WRL::ComPtr<ID3D11GeometryShader> shader;
 	};
 
 	/// <summary>
 	/// ピクセル シェーダーを表します。
 	/// </summary>
-	class PixelShader final : public GraphicsResource
+	class PixelShader final : public BaseShader<ID3D11PixelShader>
 	{
 	public:
 		PixelShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength);
 		~PixelShader() = default;
 
-		ID3D11PixelShader* GetNativePointer() const noexcept;
-
 		void Apply(ID3D11DeviceContext4* deviceContext) noexcept;
-
-	private:
-		Microsoft::WRL::ComPtr<ID3D11PixelShader> shader;
 	};
 }
--- D3D11_Sample/GameLibrary/src/Shaders.cpp
+++ D3D11_Sample/GameLibrary/src/Shaders.cpp
@@ -4,15 +4,10 @@
 using namespace GameLibrary;
 
 VertexShader::VertexShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength)
-	: GraphicsResource(graphicsDevice)
+	: BaseShader(graphicsDevice)
 {
 	ThrowIfFailed(graphicsDevice->CreateVertexShader(
 		shaderBytecode, bytecodeLength, NULL, &shader));
-}
-
-ID3D11VertexShader* VertexShader::GetNativePointer() const noexcept
-{
-	return shader.Get();
 }
 
 void VertexShader::Apply(ID3D11DeviceContext4* deviceContext) noexcept
@@ -21,15 +16,10 @@ void VertexShader::Apply(ID3D11DeviceContext4* deviceContext) noexcept
 }
 
 GeometryShader::GeometryShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength)
-	: GraphicsResource(graphicsDevice)
+	: BaseShader(graphicsDevice)
 {
 	ThrowIfFailed(graphicsDevice->CreateGeometryShader(
 		shaderBytecode, bytecodeLength, NULL, &shader));
-}
-
-ID3D11GeometryShader* GeometryShader::GetNativePointer() const noexcept
-{
-	return shader.Get();
 }
 
 void GeometryShader::Apply(ID3D11DeviceContext4* deviceContext) noexcept
@@ -38,15 +28,10 @@ void GeometryShader::Apply(ID3D11DeviceContext4* deviceContext) noexcept
 }
 
 PixelShader::PixelShader(ID3D11Device5* graphicsDevice, const void* shaderBytecode, size_t bytecodeLength)
-	: GraphicsResource(graphicsDevice)
+	: BaseShader(graphicsDevice)
 {
 	ThrowIfFailed(graphicsDevice->CreatePixelShader(
 		shaderBytecode, bytecodeLength, NULL, &shader));
-}
-
-ID3D11PixelShader* PixelShader::GetNativePointer() const noexcept
-{
-	return shader.Get();
 }
 
 void PixelShader::Apply(ID3D11DeviceContext4* deviceContext) noexcept
